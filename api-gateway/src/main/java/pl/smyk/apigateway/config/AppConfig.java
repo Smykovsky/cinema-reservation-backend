ec0f4483b5675a7a6eec6600da04515f29297690
Smykovsky <kamil.smyk00@gmail.com>
2024-08-08 13:31:32 +0200

    fix

--- api-gateway/src/main/java/pl/smyk/apigateway/config/AppConfig.java
+++ api-gateway/src/main/java/pl/smyk/apigateway/config/AppConfig.java
@@ -2,11 +2,32 @@ package pl.smyk.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 @Configuration
-public class AppConfig{
+public class AppConfig {
+    @Bean
+    public WebFilter corsWebFilter() {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            response.getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+            response.getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+            response.getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+            response.getHeaders().add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+
+            if (request.getMethod().name().equals("OPTIONS")) {
+                response.setStatusCode(HttpStatus.OK);
+                return Mono.empty();
+            }
+
+            return chain.filter(exchange);
+        };
+    }
 }
