c7d54d7e18d911ce5b4054f224767920580fcea7
kamil <kamil.smyk00@gmail.com>
2026-02-04 09:46:15 +0100

    fix: optimized auth controller and handle exception in service layer

---
+++ auth-service/src/main/java/pl/smyk/authservice/dto/ErrorResponse.java
@@ -0,0 +1,20 @@
+package pl.smyk.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+}
